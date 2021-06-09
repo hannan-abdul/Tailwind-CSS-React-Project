7546c7dbcb5a280ac43c92ffa8029e8d45700f2a
hannan-abdul <shohas563@gmail.com>
2021-06-09 21:05:04 +0600

    file update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Content from './components/Content';
+import Footer from './components/Footer';
+import { Route, Switch } from 'react-router';
+import Home from './pages'
+import Menu from './pages/menu';
+import About from './pages/about';
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
-      <Hero></Hero>
-      <Content></Content>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/menu' exact component={Menu} />
+        <Route path='/about' exact component={About} />
+      </Switch>
+      <Footer></Footer>
     </>
   );
 }
