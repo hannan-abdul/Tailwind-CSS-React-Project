cac762254bf80c216445f9362ef0ac80ab2a6315
hannan-abdul <shohas563@gmail.com>
2021-06-09 23:30:28 +0600

    code update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 This is simple landing page Tailwind CSS project
 github code link: https://github.com/hannan-abdul/Tailwind-CSS-React-Project
+Live link: https://zen-carson-870578.netlify.app/
--- src/App.js
+++ src/App.js
@@ -5,11 +5,35 @@ import { Route, Switch } from 'react-router';
 import Home from './pages'
 import Menu from './pages/menu';
 import About from './pages/about';
+import Dropdown from './components/Dropdown';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () =>{
+    setIsOpen(!isOpen);
+  }
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+        console.log('i resized');
+      }
+    };
+
+    window.addEventListener('resize', hideMenu);
+
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    };
+  });
+
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar toggle={toggle}></Navbar>
+      <Dropdown isOpen={isOpen} toggle={toggle}></Dropdown>
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/menu' exact component={Menu} />
